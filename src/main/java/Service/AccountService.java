24f13ed7e4ce7f2427eb7eb763ec1ec809cef480
Johnathan Lim <supermax123kbl@gmail.com>
2025-02-23 01:05:13 +0000

    setting up services

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,11 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountService {
+    
+}
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,11 @@
+package Service;
+
+import Model.Message;
+import DAO.MessageDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageService {
+    
+}
