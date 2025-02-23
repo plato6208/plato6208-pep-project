d8984000153d41f97fcd6c09124e0c4fda15cff6
Johnathan Lim <supermax123kbl@gmail.com>
2025-02-23 01:11:28 +0000

    working on account service

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -7,5 +7,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AccountService {
+    AccountDAO accService; 
+
+    public AccountService() {
+        accService = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO ad) {
+        this.accService = ad;
+    }
+
+    //get account, return null if no user and returns a user if it they exist
+    public Account getAccount(Account a) {
+        return null;
+    }
     
+    public Account createAccount(Account a) {
+        return null;
+    }
 }
